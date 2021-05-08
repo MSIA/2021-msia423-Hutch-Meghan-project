362c706f4024a35c897e6f6a0ae6cb4882b13974
Meghan Hutch <42354910+meghutch@users.noreply.github.com>
2021-05-07 20:48:14 -0500

    remove pre-process file

--- src/create_sample_data.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-tweets <- read.csv("data/external/constructs.csv")
-
-tweets <- head(tweets, 100) %>% 
-  select(-X2)
-
-write.csv(tweets, "data/sample/tweets.csv", 
-          row.names = FALSE)
