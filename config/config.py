96b11cb30d665eb511414dd58302c6f8728adb16
Meghan Hutch <42354910+meghutch@users.noreply.github.com>
2021-06-10 00:55:11 -0400

    update config files

--- config/config.py
+++ config/config.py
@@ -5,24 +5,14 @@ PROJECT_HOME = path.dirname(path.dirname(path.abspath(__file__)))
 print(PROJECT_HOME)
 
 # App config
-APP_NAME = "lda_tweets"
+APP_NAME = "lda-tweets"
 DEBUG = True
 
 # Logging
 LOGGING_CONFIG = path.join(PROJECT_HOME, 'config/logging.conf')
 
 # Database connection config
-#DATABASE_PATH = path.join(PROJECT_HOME, 'data/sportco.db')
-#SQLALCHEMY_DATABASE_URI = 'sqlite:////{}'.format(DATABASE_PATH)
-#SQLALCHEMY_TRACK_MODIFICATIONS = True
-#SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-
-# API configs
-#HOST = "host.docker.internal"
-#PORT = 10000
-#API_SENTIMENT_PATH = 'sentiment'
-#API_ENDPOINT = "http://{}:{}/{}".format(HOST, PORT, API_SENTIMENT_PATH)
-
-# Acquire and process config
-#MAX_RECORDS_READ = 100
-#SENTIMENT_RAW_LOCATION = path.join(PROJECT_HOME, 'data/tweet_sentiment.json')
+DATABASE_PATH = path.join(PROJECT_HOME, 'data/topics.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:////{}'.format(DATABASE_PATH)
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
